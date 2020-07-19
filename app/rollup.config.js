09d140c3d35890c55931b55d02e54d1442c51539
Jonas Galvez <jonasgalvez@gmail.com>
2020-07-18 21:35:20 -0300

    fix: ensure no dynamic imports in server build

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -1,3 +1,4 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import vue from 'rollup-plugin-vue'
 
 export default [
@@ -5,24 +6,24 @@ export default [
     input: 'src/client.js',
     output: {
       dir: 'dist',
-      format: 'esm',
+      format: 'es',
     },
     plugins: [
-      vue()
+      vue(),
+      nodeResolve()
     ]
   },
   {
     input: 'src/server.js',
     output: {
       dir: 'dist',
-      format: 'esm'
+      format: 'esm',
+      inlineDynamicImports: true,
     },
+    treeshake: false,
+    preserveEntrySignatures: false,
     plugins: [
-      vue({
-        // template: {
-        //   optimizeSSR: true,
-        // }
-      })
+      vue()
     ]
   },
 ]
